ea70994ad1241da1ae70b57918de6ed230674a95
P1rain <dsk7041@gamail.com>
2023-07-10 21:43:48 +0900

    소켓 통신 공부한 내용

--- Code/network/class_client.py
+++ Code/network/class_client.py
@@ -6,7 +6,6 @@ class ClientApp:
         # self.controller =  ClientController()
         pass
 
-
     def set_config(self, configure):
         print('클라이언트 설정 적용됨')
 
--- Code/network/class_server.py
+++ Code/network/class_server.py
@@ -1,15 +1,43 @@
 import datetime
+from socket import *
+from threading import *
 
 
 class Server:
+    HOST = '10.10.20.115'
+    PORT = '1111'
+    BUFFER = 1024
+    FORMAT = "utf-8"
+    connected_clients = {}
+
     def __init__(self):
-        pass
+        # 서버 소켓 설정
+        self.server = socket(AF_INET, SOCK_STREAM)  # AF_INET(ipv4를 의미)
+        self.server.bind((self.HOST, self.PORT))
+        self.server.listen()
 
     def set_config(self, configure):
         print('서버 설정 적용됨')
 
+    def user_add(self, user_id):
+        pass
+
     def run(self):
         print(f'서버 가동 시작 : {datetime.datetime.now()}')
+        while True:
+            client_sock, addr = self.server.accept()  # 클라이언트에 접속할때 두개의 결값을 돌려준다.
+            print(f"{addr}가 서버에 연결 되었습니다.")
+            username = client_sock.recv(self.BUFFER).decode(self.FORMAT)
+            if self.connection_check_username(username):
+                client_sock.send('연결됨'.encode(self.FORMAT))
+                new_user = dict()
+
+    def connection_check_username(self, username):
+        if self.connected_clients.get(username) == None:
+            return True
+        else:
+            return False
+
 
     def stop(self):
         print(f'서버 종료 시작 : {datetime.datetime.now()}')
