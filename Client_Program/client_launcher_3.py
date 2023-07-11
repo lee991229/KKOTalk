7647e1320b23f0eeec50eabcc251c0490612ca31
P1rain <dsk7041@gamail.com>
2023-07-11 11:19:55 +0900

    다중 로그인 테스트(완)

---
+++ Client_Program/client_launcher_3.py
@@ -0,0 +1,13 @@
+import time
+
+from Client_Program.client_config import ClientConfigure
+from Code.network.class_client_3 import ClientApp
+
+if __name__ == '__main__':
+    kkotalk_client = ClientApp()
+    configure = ClientConfigure()
+    kkotalk_client.set_config(configure)
+    kkotalk_client.start()
+
+    # time.sleep(10)
+    # kkotalk_client.exit()
