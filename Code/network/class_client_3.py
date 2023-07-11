1094a7b94df588509a5694cd5f1c05a7bbdafacd
P1rain <dsk7041@gamail.com>
2023-07-11 21:05:13 +0900

    최신화

--- Code/network/class_client_3.py
+++ Code/network/class_client_3.py
@@ -5,7 +5,7 @@ from threading import *
 
 
 class ClientApp:
-    HOST = '10.10.20.115'
+    HOST = '127.0.0.1'
     PORT = 9999
     BUFFER = 1024
     FORMAT = "utf-8"
@@ -15,39 +15,59 @@ class ClientApp:
         # self.controller =  ClientController()
         self.connected = False
         self.client = socket(AF_INET, SOCK_STREAM)
-        # self.receive_thread = Thread(target=self.receive_message)
+        # 아직 쓴 이유를 모르겠음 일단 필요할것 같음
         self.connect_to_surver()
         # 임의로 지정
         self.user_id = 'test3'
         self.user_pw = '123'
+        self.send_thread = Thread(target=self.send_message)
+
+        self.receive_thread = Thread(target=self.receive_message)
+
         self.validate_user(self.user_id, self.user_pw)
 
     def set_config(self, configure):
         print('클라이언트 설정 적용됨')
 
+    # 소캣이 서버에 접촉
     def connect_to_surver(self):
         self.client.connect(self.SERVER)
 
     def validate_user(self, user_id, user_pw):
-        self.client.send(user_id.encode(self.FORMAT))
-        if self.client.recv(self.BUFFER).decode(self.FORMAT) == '아이디존재':
-            print("아이디 일치")
-            self.client.send(user_pw.encode(self.FORMAT))
-        if self.client.recv(self.BUFFER).decode(self.FORMAT) == "연결됨":
-            self.connected = True
-            print("로그인 됨")
-            while True:
-                pass
-            # 로그인되면 크크오톡 화면 뜨
+        client = self.client
+        format_ = self.FORMAT
+        buffer = self.BUFFER
+        # 아이디 전송
+        client.send(user_id.encode(format_))
+        if client.recv(buffer).decode(format_) == '아이디존재':
+            # 비밀번호 전송
+            client.send(user_pw.encode(format_))
+            if client.recv(buffer).decode(format_) == "연결됨":
+                # 연결상태로 변경
+                self.connected = True
+                print('도착')
+                self.receive_thread.start()
+                self.send_thread.start()
+                while True:
+                    pass
+                # 화면 변경되면서 채팅바응로 들어가진다.
+
             # 로그인되면 크크오톡 화면 뜨는건가? UI뜨게 변경되는 느낌인가?
             # self.receive_thread.start()
 
-    # def send_message(self):
-    #     message = self.textEdit.toPlainText().strip()  # 이건 뭔지 아직 이해 못했음
-    #     if message:
-    #         message_date = datetime.datetime.now()
-    #         self.client.send(message.encode(self.FORMAT))
-    #         self.
+    def send_message(self):
+        while True:
+            # 메시지 전송
+            message = input()
+            self.client.send(message.encode(self.FORMAT))
+            # 보내진 아이디 받기
+            message = self.client.recv(self.BUFFER).decode(self.FORMAT)
+            print(message)
+
+    def receive_message(self):
+        while True:
+            message = self.client.recv(self.BUFFER).decode(self.FORMAT)
+            print(message)
 
     def start(self):
         print(f'클라이언트 프로그램 가동 시작 : {datetime.datetime.now()}')
--- Code/network/class_server.py
+++ Code/network/class_server.py
@@ -1,15 +1,17 @@
 import datetime
+import threading
 from socket import *
 from threading import *
 
 
 class Server:
-    HOST = '10.10.20.115'
+    HOST = '127.0.0.1'
     PORT = 9999
     BUFFER = 1024
     FORMAT = "utf-8"
     connected_clients_id = ['test1', 'test2', 'test3']
     connected_user = [('test1', 'password'), ('test2', 'password1'), ('test3', '123')]
+    login_user = []
 
     def __init__(self):
         # 서버 소켓 설정
@@ -23,18 +25,40 @@ class Server:
     def user_add(self, user_id):
         pass
 
+    # 일단 대충 만들어 놓은것
+    def server_status(self, status):
+        # ready = 0, run = 1, close = -1
+        if status == 0:
+            print('준비중')
+        elif status == 1:
+            print('가동중')
+        elif status == -1:
+            print('종료중')
+
     def run(self):
         print(f'서버 가동 시작 : {datetime.datetime.now()}')
         while True:
             client_sock, addr = self.server.accept()  # 클라이언트에 접속할때 두개의 결값을 돌려준다. ip주소랑, 클라이언트 소켓
             print(f"{addr}가 서버에 연결 되었습니다.")
+            # 로그인 기능
             user_id = client_sock.recv(self.BUFFER).decode(self.FORMAT)
             if self.validate_id(user_id):
                 client_sock.send('아이디존재'.encode(self.FORMAT))
                 user_pw = client_sock.recv(self.BUFFER).decode(self.FORMAT)
                 if self.validate_pw(user_id, user_pw):
+                    # 로그인 상태의 아이디를 리스트에 추가
+                    self.login_user.append(client_sock)
                     client_sock.send('연결됨'.encode(self.FORMAT))
                     print(f'{user_id}가 접속')
+                    # 로그인이 성공하면 톡방 정보랑 계정 정보들?을 전종해줘야한다.
+                    # 메시지의 동작이 있을떄 작동하는 듯?
+                else:
+                    # 로그인 실패시임
+                    print('로그인 실패')
+            else:
+                print('로그인 실패')
+            client_thread = threading.Thread(target=self.message_handler, args=(user_id, addr, client_sock))
+            client_thread.start()
             # if self.validate_username(user_id):
             #     client_sock.send('연결됨'.encode(self.FORMAT))
 
@@ -53,12 +77,13 @@ class Server:
         else:
             return False
 
-    def validate_username(self, username):
-        if username in self.connected_clients:
-            return True
-        else:
-            return False
-
+    def message_handler(self, user_id, addr, client_sock):
+        connect = True
+        while connect:
+            message = client_sock.recv(self.BUFFER).decode(self.FORMAT)
+            message_date = datetime.datetime.now()
+            for i in self.login_user:
+                i.send(f'{user_id} : {message} > 시간{message_date}'.encode(self.FORMAT))
 
     def stop(self):
         print(f'서버 종료 시작 : {datetime.datetime.now()}')
