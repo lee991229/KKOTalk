2046c258d2d037ce9904a5a4f4838221acc57c76
kdt100 <alfajeze@gmail.com>
2023-07-11 12:38:09 +0900

    crud message테이블까지 추가

--- Code/domain/class_long_contents.py
+++ Code/domain/class_long_contents.py
@@ -1,7 +1,7 @@
 
 class LongContents:
-    def __init__(self, contents_id, type, long_text, image):
+    def __init__(self, contents_id, contents_type, long_text, image):
         self.contents_id = contents_id
-        self.type = type
+        self.contents_type = contents_type
         self.long_text = long_text
         self.image = image
