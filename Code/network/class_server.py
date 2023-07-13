b3748dbf66d00a688de421d782d63d081cca20cc
EON <mun0392@gmail.com>
2023-07-13 19:40:56 +0900

    server를 합침

--- Code/network/class_server.py
+++ Code/network/class_server.py
@@ -1,65 +1,112 @@
-import datetime
+import os
+from multiprocessing import Process
 from socket import *
-from threading import *
+from threading import Thread, Event
+
+import select
+
+from Code.domain.class_db_connector import DBConnector
 
 
 class Server:
-    # HOST = '10.10.20.115'
     HOST = '127.0.0.1'
     PORT = 9999
     BUFFER = 1024
     FORMAT = "utf-8"
-    connected_clients_id = ['test1', 'test2', 'test3']
-    connected_user = [('test1', 'password'), ('test2', 'password1'), ('test3', '123')]
+    HEADER_LENGTH = 30
 
-    def __init__(self):
+    assert_username = f"{'assert_username':<{HEADER_LENGTH}}"
+    join_user = f"{'join_user':<{HEADER_LENGTH}}"
+    login = "login"
+    send_msg_c_room = "send_msg_c_room"
+    send_alarm_c_room = "send_alarm_c_room"
+    pass_encoded = f"{'pass':<{1024-HEADER_LENGTH}}".encode(FORMAT)
+    dot_encoded = f"{'.':<{1024-HEADER_LENGTH}}".encode(FORMAT)
+
+    HEADER_LIST = {
+        assert_username: assert_username.encode(FORMAT),
+        join_user: join_user.encode(FORMAT),
+        login: login.encode(FORMAT),
+        send_msg_c_room: send_msg_c_room.encode(FORMAT),
+        send_alarm_c_room: send_alarm_c_room.encode(FORMAT),
+    }
+
+    def __init__(self, db_conn:DBConnector):
         # 서버 소켓 설정
-        self.server = socket(AF_INET, SOCK_STREAM)  # AF_INET(ipv4를 의미)
-        self.server.bind((self.HOST, self.PORT))
-        self.server.listen()
+        self.db_conn = db_conn
+        self.server_socket = None
+        self.config = None
+        self.sockets_list = list()
+        self.clients = dict()
+        self.thread_for_run = None
+        self.run_signal = True
 
     def set_config(self, configure):
+        self.config = configure
         print('서버 설정 적용됨')
 
-    def user_add(self, user_id):
-        pass
-
-    def run(self):
-        print(f'서버 가동 시작 : {datetime.datetime.now()}')
-        while True:
-            client_sock, addr = self.server.accept()  # 클라이언트에 접속할때 두개의 결값을 돌려준다. ip주소랑, 클라이언트 소켓
-            print(f"{addr}가 서버에 연결 되었습니다.")
-            user_id = client_sock.recv(self.BUFFER).decode(self.FORMAT)
-            if self.validate_id(user_id):
-                client_sock.send('아이디존재'.encode(self.FORMAT))
-                user_pw = client_sock.recv(self.BUFFER).decode(self.FORMAT)
-                if self.validate_pw(user_id, user_pw):
-                    client_sock.send('연결됨'.encode(self.FORMAT))
-                    print(f'{user_id}가 접속')
-            # if self.validate_username(user_id):
-            #     client_sock.send('연결됨'.encode(self.FORMAT))
-
-    # 아이디 확인
-    def validate_id(self, user_id):
-        if user_id in self.connected_clients_id:
-            return True
-        else:
-            return False
-
-    # 비밀번호 아이디 일치하는지 확인
-    def validate_pw(self, user_id, user_pw):
-        user_info = (user_id, user_pw)
-        if user_info in self.connected_user:
-            return True
-        else:
-            return False
-
-    def validate_username(self, username):
-        if username in self.connected_clients:
-            return True
-        else:
-            return False
-
+    def start(self):
+        if self.thread_for_run is not None:  # 실행중이면 종료 시키기
+            return
+        self.server_socket = socket(AF_INET, SOCK_STREAM)  # AF_INET(ipv4를 의미)
+        self.server_socket.bind((self.HOST, self.PORT))  # 바인딩
+        self.server_socket.listen()  # 리슨 시작
+        self.sockets_list.clear()  # 소켓리스트 클리어
+        self.sockets_list.append(self.server_socket)
+        self.run_signal = True
+        self.thread_for_run = Thread(target=self.run)
+        self.thread_for_run.start()
 
     def stop(self):
-        print(f'서버 종료 시작 : {datetime.datetime.now()}')
+        self.run_signal = False
+        if self.thread_for_run is not None:
+            self.thread_for_run.join()
+        self.server_socket.close()
+        self.thread_for_run = None
+
+    def run(self):
+        while True:
+            if self.run_signal is False:
+                break
+            try:
+                read_sockets, _, exception_sockets = select.select(self.sockets_list, [], self.sockets_list, 0.1)
+            except Exception:
+                continue
+            for notified_socket in read_sockets:
+                if notified_socket == self.server_socket:
+                    client_socket, client_address = self.server_socket.accept()
+                    user = self.receive_message(client_socket)
+                    if user is False:
+                        continue
+                    self.sockets_list.append(client_socket)
+                    self.clients[client_socket] = user
+
+                else:
+                    message = self.receive_message(notified_socket)
+
+                    if message is False:
+                        self.sockets_list.remove(notified_socket)
+                        del self.clients[notified_socket]
+                        continue
+
+
+            for notified_socket in exception_sockets:
+                self.sockets_list.remove(notified_socket)
+                del self.clients[notified_socket]
+
+    def receive_message(self, client_socket:socket):
+        try:
+            recv_message = client_socket.recv(self.BUFFER)
+            request_header = recv_message[:self.HEADER_LENGTH].strip().decode(self.FORMAT)
+            request_data = recv_message[self.HEADER_LENGTH:].strip().decode(self.FORMAT)
+            # 아이디 중복
+            if request_header == self.assert_username.strip():
+                result = self.db_conn.assert_same_login_id(request_data)
+                if result is True:
+                    response_header = self.assert_username.encode(self.FORMAT)
+                    client_socket.send(response_header + self.pass_encoded)
+            # 회원가입
+            elif request_header == self.join_user.strip():
+                result = self.db_conn.user_sign_up()
+        except:
+            return False
--- Test/s_server_test.py
+++ Test/s_server_test.py
@@ -3,12 +3,8 @@ import sys
 from PyQt5.QtWidgets import QApplication
 
 from Code.domain.class_db_connector import DBConnector
-from Code.front.class_client_controller import WindowController
-from Code.network.class_client_prototype import ClientPrototypeWidget
 from Code.network.class_server import Server
-from Code.network.class_server_2 import Server2
 from Code.network.class_server_controller_widget import ServerControllerWidget
-from Code.network.class_server_fix_head import ServerFixHead
 from Common.common_module import *
 
 
@@ -19,8 +15,7 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     db_conn = DBConnector(test_option=True)
     db_conn.create_tables()
-    # server = Server2(db_conn)
-    server = ServerFixHead(db_conn)
+    server = Server(db_conn)
     proto_widget = ServerControllerWidget(server, db_conn)
     proto_widget.show()
 
