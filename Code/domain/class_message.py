42f5a7ad82a0915d67d3726192afde1c4fd6e42f
P1rain <dsk7041@gmail.com>
2023-07-15 05:10:55 +0900

    저장된 메세지 반환 기능 빼고 전부 작성  초대기능, 방 만드는 기능 확인 못함 실험해봐야함

--- Code/domain/class_message.py
+++ Code/domain/class_message.py
@@ -2,12 +2,12 @@ import json
 
 
 class Message:
-    def __init__(self, message_id, sender_user_id, talk_room_id, send_time_stamp, contents, long_contents_id, user_obj=None):
+    def __init__(self, message_id, sender_user_id, talk_room_id, contents, send_time_stamp, long_contents_id, user_obj=None):
         self.message_id = message_id
         self.sender_user_id = sender_user_id
         self.talk_room_id = talk_room_id
-        self.send_time_stamp = send_time_stamp
         self.contents = contents
+        self.send_time_stamp = send_time_stamp
         self.long_contents_id = long_contents_id
         self.user_obj = user_obj
 
